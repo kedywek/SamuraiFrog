8701a5386e68580f14ab83409542e299c7d1ba8d
251488 Boss Krzysztof <251488@edu.p.lodz.pl>
2024-10-29 17:38:05 +0100

    Add jump stages

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 var run_speed = 400.0
 var acceleration = 0.1
 var deceleration = 0.1
-const jump = -600.0
+const jump_height1 = -800.0
+const jump_height2 = -600.0
+const jump_height3 = -400.0
 
 var dash_speed = 1100
 var dash_max_distance = 1000
@@ -20,25 +22,33 @@ enum States {Idle, Run, Charge, Jump, Attack}
 var state = States.Idle
 var direction
 
+var time_left = 0
+
 
 func _process(_delta: float) -> void:
 	flip_sprite()
 	
-	if $ProgressBar.value == 100:
-		$ProgressBar.visible = false
+	if $jump_regen.value == 100:
+		$jump_regen.visible = false
 	else:
-		$ProgressBar.visible = true
+		$jump_regen.visible = true
+		
 	
-	$ProgressBar.set_value((1 - $dash_cooldown.time_left)*100)
+	if $jump_timer.is_stopped() or $jump_charge.value == 0:
+		$jump_charge.visible = false
+	else:
+		$jump_charge.visible = true
 	
+	$jump_regen.set_value((1 - $dash_cooldown.time_left)*100)
 	
+	$jump_charge.set_value((2 - $jump_timer.time_left)*100)
 	
 
 
 func _physics_process(delta: float) -> void:
 
 	if not is_on_floor(): #grawitacja
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.3
 
 	handle_state_transitions()
 	
@@ -52,11 +62,20 @@ func handle_state_transitions():
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		state = States.Charge
+		$jump_timer.start()
 		
 		
 	if state == States.Charge and Input.is_action_just_released("jump"):
+		time_left=$jump_timer.time_left
+		$jump_timer.stop()
+		print(time_left)
 		state = States.Jump
-		velocity.y = jump
+		if(2.0 > time_left and time_left >= 1.5):
+			velocity.y = jump_height3
+		elif (1.5 > time_left and time_left >= 1.0):
+			velocity.y = jump_height2
+		else:
+			velocity.y = jump_height1
 		
 	if direction != 0 and state != States.Charge:
 		state = States.Run
@@ -70,7 +89,7 @@ func handle_state_transitions():
 		
 	if !is_on_floor() and state == States.Jump and not is_dashing and can_dash and Input.is_action_just_pressed("attack") and direction:
 		state = States.Attack
-		#print("chuj")
+
 		
 		
 		
