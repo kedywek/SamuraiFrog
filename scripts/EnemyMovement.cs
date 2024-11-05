a104c5e14da85e0629f63fbc48c92dabb7662a82
Jasek Damian <damiannq1337@gmail.com>
2024-11-05 22:40:00 +0100

    Add working movement

--- scripts/EnemyMovement.cs
+++ scripts/EnemyMovement.cs
@@ -1,22 +1,42 @@
 using Godot;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+
+namespace SamuraiFrog.scripts;
 
 public partial class EnemyMovement : CharacterBody2D
 {
-	[Export] 
-	private float _speed = 10f;
+	[Export] private float _speed;
+	[Export] private float _stoppingDistance;
 	private Node2D _player;
+	private Vector2 _targetVelocity = Vector2.Zero;
+	private Vector2 _basePosition;
+
 	public override void _Ready()
 	{
 		_player =  GetTree().GetNodesInGroup("Player")[0] as Node2D;
+		_basePosition = Position;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 directionTo = Position.DirectionTo(_player.Position).Normalized();
-		Velocity = directionTo * _speed;
+		GD.Print(Position.DistanceTo(_player.Position));
+		if (Position.DistanceTo(_player.Position) >= _stoppingDistance) {
+			_targetVelocity.X = GetDirection(_player.Position).X * _speed;
+		}
+		else {
+			_targetVelocity.X = 0f;
+		}
+		Velocity = _targetVelocity;
 		MoveAndSlide();
+	}
+
+	private void OnEnterArea2D(Node2D node) {
+	}
+
+	private void OnExitArea2D(Node2D node) {
+	}
+
+	private Vector2 GetDirection(Vector2 target) {
+		Vector2 dir = Position.DirectionTo(target).Normalized();
+		return dir;
 	}
 }
